b8be589306166d0c51e88045204bf5ffc2a764ac
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-11-01 09:38:09 +0200

    4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -11,9 +11,15 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (!node)
 		return (0);
+	/*
 	if (!node->parent)
 		return (0);
 	if (node->left || node->right)
 		return (0);
 	return (1);
+	*/
+	if (node->parent && !node->left && !node->right)
+		return (1);
+	else
+		return (0);
 }
