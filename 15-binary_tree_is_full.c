4be322b11e1f7931f185139588ae8c75d94b2a98
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-11-03 04:01:05 +0200

    15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,11 +7,16 @@
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	int right, left;
 	if (!tree)
 		return (0);
 	if (!tree->right && !tree->left)
 		return (1);
 	if (tree->right && tree->left)
-		return (binary_tree_is_full(tree->right) && binary_tree_is_full(tree->left));
+	{
+		right = binary_tree_is_full(tree->right);
+		left = binary_tree_is_full(tree->left);
+		return (right && left);
+	}
 	return (0);
 }
