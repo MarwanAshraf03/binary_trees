0edb1410df43d51f18ca9742ba6e89c32e09c470
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-11-03 02:29:02 +0200

    15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,31 +7,53 @@
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int right, left;
+	int n_leaves, height, i = 0, new = 1;
 
 	if (!tree)
 		return (0);
-	if (!(tree->right && tree->left))
-		return (0);
-	left = extra_full(tree->left);
-	right = extra_full(tree->right);
-	return ((left > right) ? right : left);
+	n_leaves = extra_leaves(tree);
+	height = extra_height(tree, 0);
+	for (i = 0; i < height; i++)
+		new *= 2;
+	if (new == n_leaves)
+		return (1);
+	return (0);
 }
 
 /**
- * extra_full - helper function
- * @tree: root node
- * Return: 1 if subtree is full, 0 otherwise
+ * extra_height - an extra function to help me use counter
+ * @tree: the node to be used to calculate height
+ * @count: the count (height) of @tree
+ * Return: returns the height of left or right node (the bigger one)
 */
-int extra_full(binary_tree_t *tree)
+size_t extra_height(const binary_tree_t *tree, size_t count)
 {
-	int help = (tree->left != NULL) ^ (tree->right != NULL);
+	size_t left_count = count, right_count = count;
 
-	if (help)
-		return (0);
 	if (tree->left)
-		extra_full(tree->left);
+	{
+		left_count++;
+		left_count = extra_height(tree->left, left_count);
+	}
 	if (tree->right)
-		extra_full(tree->right);
-	return (1);
+	{
+		right_count++;
+		right_count = extra_height(tree->right, right_count);
+	}
+	return ((right_count > left_count) ? right_count : left_count);
+}
+
+
+/**
+ * extra_leaves - an extra function to help me use counter
+ * @tree: the node to be used to calculate height
+ * Return: returns the height of left or right node (the bigger one)
+*/
+size_t extra_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1 + extra_leaves(tree->left) + extra_leaves(tree->right));
+	return (0 + extra_leaves(tree->left) + extra_leaves(tree->right));
 }
