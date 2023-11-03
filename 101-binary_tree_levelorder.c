e14e2a917ace3d2af13bec7257da2adf92ca04e0
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-11-03 22:57:04 +0200

    101-binary_tree_levelorder.c

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -7,10 +7,13 @@
 */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
+	int height, i;
+
 	if (!tree || !func)
 		return;
-	func(tree->n);
-	extra_level_order(tree, func);
+	height = extra_height(tree, 1);
+	for (i = 1; i <= height; i++)
+		print_level(tree, func, i);
 }
 
 /**
@@ -28,4 +31,44 @@ void extra_level_order(const binary_tree_t *tree, void (*func)(int))
 	extra_level_order(tree->left, func);
 	if (tree->right)
 	extra_level_order(tree->right, func);
+}
+
+/**
+ * print_level - helper function to print level of tree
+ * @tree: root node
+ * @func: function printing node value
+ * @level: level to be printed
+*/
+void print_level(const binary_tree_t *tree, void (*func)(int), int level)
+{
+	if (level == 1)
+		func(tree->n);
+	else
+	{
+		print_level(tree->left, func, level - 1);
+		print_level(tree->right, func, level - 1);
+	}
+}
+
+/**
+ * extra_height - an extra function to help me use counter
+ * @tree: the node to be used to calculate height
+ * @count: the count (height) of @tree
+ * Return: returns the height of left or right node (the bigger one)
+*/
+size_t extra_height(const binary_tree_t *tree, size_t count)
+{
+	size_t left_count = count, right_count = count;
+
+	if (tree->left)
+	{
+		left_count++;
+		left_count = extra_height(tree->left, left_count);
+	}
+	if (tree->right)
+	{
+		right_count++;
+		right_count = extra_height(tree->right, right_count);
+	}
+	return ((right_count > left_count) ? right_count : left_count);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -51,6 +51,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 void extra_level_order(const binary_tree_t *tree, void (*func)(int));
+void print_level(const binary_tree_t *tree, void (*func)(int), int level);
 
 
 #endif
